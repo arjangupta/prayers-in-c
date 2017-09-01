f7f0e166bb139065b273a138d2d945d85456210c
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-01 16:25:29 -0500

    Write a more correct implementation of the function in question.

--- strncmp/strncmp.c
+++ strncmp/strncmp.c
@@ -17,19 +17,15 @@ int strncmp_a (const char *s1, const char *s2, size_t n)
 	 * 2. If ret > 0, then s2 is smaller than s1.
 	 * 3. If ret == 0, then s1 and s2 are the same size.
 	 */
-	int ret = 0;
-	for (size_t i = 0; i < n; ++i)
+	if (n == 0)
+		return n;
+
+	size_t i = 0;
+	while (i < n && s1[i] == s2[i]) 
 	{
-		if (s1[i] > s2[i])
-		{
-			++ret;
-		} else if (s1[i] < s2[i]) {
-			--ret;
-		} else {
-			//do not alter the value of ret.
-		}
+		++i;
 	}
-	return ret;
+	return (s1[i] - s2[i]);
 }
 
 int main ()
@@ -42,23 +38,23 @@ int main ()
 	 */
 
 	/* USE CASE 1 */
-	char str1[5] = "Okay!";
-	char str2[3] = "Use";
+	char str1[] = "Okay!";
+	char str2[] = "Use\0";
 	printf("Use case 1 return value: %d\n", strncmp_a(str1, str2, 0));
 
 	/* USE CASE 2 */
-	char str3[4] = "Nice";
-	char str4[4] = "COOL";
+	char str3[] = "Nice";
+	char str4[] = "COOL";
 	printf("Use case 2 return value: %d\n", strncmp_a(str3, str4, 4));
 
 	/* USE CASE 3 */
-	char str5[4] = "Hell";
-	char str6[5] = "Hello";
+	char str5[] = "Hell";
+	char str6[] = "Hello";
 	printf("Use case 3 return value: %d\n", strncmp_a(str5, str6, 5)); /* wait, why does this happen? */
 
 	/* USE CASE 4 */
-	char str7[2] = "ha";
-	char str8[2] = "ah";
+	char str7[] = "ha";
+	char str8[] = "ah";
 	printf("Use case 4 return value: %d\n", strncmp_a(str7, str8, 2));
 
 	return 0;
