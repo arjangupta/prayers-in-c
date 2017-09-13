24160e7e52934671597e536ec41330d557993b2d
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-13 16:28:31 +0800

    Write the divide() function.

--- merge-sort/merge_sort.c
+++ merge-sort/merge_sort.c
@@ -5,15 +5,36 @@
 
 #include <stdio.h>
 
+void divide (int* arr, int len)
+{
+	if (len == 1)
+	{
+		return;
+	}
+
+	divide (arr[len/2], len/2);
+	divide (arr [0], (len - len/2) );
+
+	//merge (arr[0], arr[len/2], )
+}
+
+void mergeSort (int* arr, int len)
+{
+	divide(arr, len);
+}
+
 int main()
 {
+	/* printf("%d", 5/2); */
+
 	int arr[] = {1, 2, 3, 4, 8, 6};
 	int numelems = (int)( sizeof(arr) / sizeof(arr[0]) );
-	//printf("%d", numelems);
+	/* printf("%d", numelems); */
 
-	//call mergesort() on arr
+	/* call merge sort on arr */
+	mergeSort(arr, numelems);
 
-	//check if arr is sorted
+	/* check if arr is sorted */
 	for (size_t i = 0; i < (numelems - 1); ++i)
 	{
 		if (arr[i] > arr[i + 1])
