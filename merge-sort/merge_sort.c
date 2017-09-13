0f55e0d03d7b02dd0b34f472ac0c16d5ee4e39ff
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-14 01:50:12 +0800

    Implement merge() function to work correctly.

--- merge-sort/merge_sort.c
+++ merge-sort/merge_sort.c
@@ -7,18 +7,69 @@
 
 void merge (int *arrL, int lenL, int *arrR, int lenR)
 {
+	for (size_t i = 0; i < lenL; ++i)
+	{
+		printf("%d ", arrL[i]);
+	}
 
+	printf("\n");
+
+	for (size_t i = 0; i < lenR; ++i)
+	{
+		printf("%d ", arrR[i]);
+	}
+
+	printf("\n");
+
+	int buffArr[lenL + lenR];
+	size_t j = 0; 
+	size_t k = 0;
+	for (size_t i = 0; i < (lenL + lenR); ++i)
+	{
+		if (j == lenL)
+		{
+			buffArr[i] = arrR[k];
+			++k;
+		} else if (k == lenR) {
+			buffArr[i] = arrL[j];
+			++j;
+		} else {
+			if (arrL[j] < arrR[k])
+			{
+				buffArr[i] = arrL[j];
+				++j;
+			} else {
+				buffArr[i] = arrR[k];
+				++k;
+			}
+		}
+	}
+
+	/*arrL = buffArr;
+	 This line of code didn't work, most probably because 
+	 buffArr goes out of scope at the end of this function and
+	 the fact that the pointer arrL was passed in by copy.*/
+
+	for (size_t i = 0; i < (lenL + lenR); ++i)
+	{
+		/* the next line of code actually 
+		changes the memory being pointed to */
+		arrL[i] = buffArr[i]; 
+		printf("%d ", arrL[i]);
+	}
+
+	printf("\n");
 }
 
 void divide (int* arr, int len)
 {
 
-	for (size_t i = 0; i < len; ++i)
+	/*for (size_t i = 0; i < len; ++i)
 	{
 		printf("%d ", arr[i]);
 	}
 
-	printf("\n");
+	printf("\n");*/
 
 	if (len == 1)
 	{
@@ -33,19 +84,19 @@ void divide (int* arr, int len)
 
 void mergeSort (int* arr, int len)
 {
-	//divide(arr, len);
+	/*divide(arr, len);*/
 	merge(arr, len/2, &arr[len/2], (len - len/2));
 }
 
 int main()
 {
-	/* printf("%d", 5/2); */
+	/*printf("%d", 5/2);*/
 
 	int arr[] = {1, 2, 3, 1, 2, 3};
-	/*int arr[] = {1, 2, 3, 4, 8, 6, 10, 9, 7, 5}; */
-	/* int arr[] = {8, 6, 34, 23, 4, 3}; */
+	/*int arr[] = {1, 2, 3, 4, 8, 6, 10, 9, 7, 5};*/
+	/*int arr[] = {8, 6, 34, 23, 4, 3};*/
 	int numelems = (int)( sizeof(arr) / sizeof(arr[0]) );
-	/* printf("%d", numelems); */
+	/*printf("%d", numelems);*/
 
 	/* call merge sort on arr */
 	mergeSort(arr, numelems);
@@ -64,6 +115,13 @@ int main()
 			printf("Array is sorted\n");
 		}
 	}
+
+	for (size_t i = 0; i < numelems; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
 	
 	return 0;
 }
