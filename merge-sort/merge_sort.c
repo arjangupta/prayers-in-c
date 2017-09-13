1b99e1a3b103a37fac8c403d726a99c09a44ac87
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-13 17:02:15 +0800

    Begin implementation of merge() function.

--- merge-sort/merge_sort.c
+++ merge-sort/merge_sort.c
@@ -5,29 +5,45 @@
 
 #include <stdio.h>
 
+void merge (int *arrL, int lenL, int *arrR, int lenR)
+{
+
+}
+
 void divide (int* arr, int len)
 {
+
+	for (size_t i = 0; i < len; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+
 	if (len == 1)
 	{
 		return;
 	}
 
-	divide (arr[len/2], len/2);
-	divide (arr [0], (len - len/2) );
+	divide(&arr[len/2], (len - len/2));
+	divide(arr, len/2);
 
-	//merge (arr[0], arr[len/2], )
+	merge(arr, len/2, &arr[len/2], (len - len/2));
 }
 
 void mergeSort (int* arr, int len)
 {
-	divide(arr, len);
+	//divide(arr, len);
+	merge(arr, len/2, &arr[len/2], (len - len/2));
 }
 
 int main()
 {
 	/* printf("%d", 5/2); */
 
-	int arr[] = {1, 2, 3, 4, 8, 6};
+	int arr[] = {1, 2, 3, 1, 2, 3};
+	/*int arr[] = {1, 2, 3, 4, 8, 6, 10, 9, 7, 5}; */
+	/* int arr[] = {8, 6, 34, 23, 4, 3}; */
 	int numelems = (int)( sizeof(arr) / sizeof(arr[0]) );
 	/* printf("%d", numelems); */
 
