7dce1d7e4b87aa4fcf485d28662e32fe212c6966
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-16 02:37:47 +0800

    Refactor the merge function to work with int** parms and successfully get the sort to start working with higher efficiency.

--- merge-sort/merge_sort.c
+++ merge-sort/merge_sort.c
@@ -6,67 +6,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge (int *arrL, int lenL, int *arrR, int lenR)
+void merge (int** arrLPtr, int lenL, int** arrRPtr, int lenR)
 {
 	/*for (size_t i = 0; i < lenL; ++i)
 	{
-		printf("%d ", arrL[i]);
+		printf("%d ", *(*arrLPtr + i));
 	}
 
 	printf("\n");
 
 	for (size_t i = 0; i < lenR; ++i)
 	{
-		printf("%d ", arrR[i]);
+		printf("%d ", *(*arrRPtr + i));
 	}
 
 	printf("\n");*/
 
-	int buffArr[lenL + lenR];
+	int* buffArr = (int *) malloc( (lenL + lenR) * sizeof(int) );
 	size_t j = 0; 
 	size_t k = 0;
 	for (size_t i = 0; i < (lenL + lenR); ++i)
 	{
 		if (j == lenL)
+			buffArr[i] = *(*arrRPtr + k++);
+		else if (k == lenR)
+			buffArr[i] = *(*arrLPtr + j++);
+		else 
 		{
-			buffArr[i] = arrR[k];
-			++k;
-		} else if (k == lenR) {
-			buffArr[i] = arrL[j];
-			++j;
-		} else {
-			if (arrL[j] < arrR[k])
-			{
-				buffArr[i] = arrL[j];
-				++j;
-			} else {
-				buffArr[i] = arrR[k];
-				++k;
-			}
+			if (*(*arrLPtr + j) < *(*arrRPtr + k))
+				buffArr[i] = *(*arrLPtr + j++);
+			else
+				buffArr[i] = *(*arrRPtr + k++);
 		}
 	}
 
-	/*arrL = buffArr;
-	 The above line of code didn't work, most probably because 
-	 buffArr goes out of scope at the end of this function and
-	 the fact that the pointer arrL was passed in by copy.*/
-
-	for (size_t i = 0; i < (lenL + lenR); ++i)
-	{
-		/* the next line of code actually 
-		changes the memory being pointed to */
-		arrL[i] = buffArr[i];
-	}
+	*arrLPtr = buffArr;
 }
 
 void divide (int** arrPtr, int len)
 {
-	for (size_t i = 0; i < len; ++i)
+	/*for (size_t i = 0; i < len; ++i)
 	{
 		printf("%d ", *(*arrPtr + i));
 	}
 
-	printf("\n");
+	printf("\n");*/
 
 	if (len == 1)
 	{
@@ -77,7 +61,7 @@ void divide (int** arrPtr, int len)
 	divide(&leftArr, (len - len/2));
 	divide(arrPtr, len/2);
 
-	/*merge(arr, len/2, &arr[len/2], (len - len/2));*/
+	merge(arrPtr, len/2, &leftArr, (len - len/2));
 }
 
 void mergeSort (int** arrPtr, int len)
@@ -97,23 +81,14 @@ int main()
 
 	int *arr = (int *) malloc( numelems * sizeof(int) );
 
-	/*arr[0] = 0;
-	arr[1] = 1;
-	arr[2] = 2;
-	arr[3] = 3;
-	arr[4] = 4;
-	arr[5] = 5;
-	arr[6] = 6;
-	arr[7] = 7;*/
-
 	/*int numelems = (int)( sizeof(arr[]) / sizeof(arr[0]) );*/
-	printf("\n%d\n", numelems);
+	printf("\nSize of array is %d\n", numelems);
 
 	/* show the user the initial list */
 	printf("\nInitial list: ");
 	for (size_t i = 0; i < numelems; ++i)
 	{
-		arr[i] = i % 5;
+		arr[i] = i % 3;
 		printf("%d ", arr[i]);
 	}
 
