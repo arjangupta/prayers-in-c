53109529d36c87e9c9423f78d73091319a6d6a95
Arjan Gupta <arjan.gupta@eyeverify.com>
2017-09-26 15:46:30 -0500

    Implemented an example program that demonstrates the usage of static keyword.

--- static-usage/static_usage.c
+++ static-usage/static_usage.c
@@ -3,16 +3,39 @@
  * Purpose: Understand the usage of static
  */
 
-void foo()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void example()
 {
 	/* declare a static var */
-	
+	static char* stChArr = "Hello";
 
+	/* declare a non-static var for comparing the results */
+	char* plainChArr = "Hello";
+
+	printf("\n");
+
+	printf("The static character array is: %s\n", stChArr);
+	printf("The plain character array is: %s\n", plainChArr);
+
+	printf("\nReset the pointer locations.\n\n");
+
+	stChArr = stChArr + 1;
+	plainChArr = plainChArr + 1;
+
+	printf("The static character array is: %s\n", stChArr);
+	printf("The plain character array is: %s\n", plainChArr);
 }
 
 
 int main()
 {
-	foo();
+	for (size_t i = 0; i < 4; ++i)
+	{
+		printf("\nITERATION #%zu\n", (i + 1) );
+		example();
+	}
 	return 0;
 }
