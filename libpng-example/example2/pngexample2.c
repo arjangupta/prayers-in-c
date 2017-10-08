70c671d578c8efa4acc1ed4ba70185d6886edbf9
Gupta, Arjan <arjan.gupta@alibaba-inc.com>
2017-10-08 12:48:39 -0500

    Rename .cpp to .c

--- libpng-example/example2/pngexample2.c
+++ libpng-example/example2/pngexample2.c
@@ -1,9 +1,9 @@
-#include "libpng-1.6.32/png.h"
+#include "../libpng-1.6.32/png.h"
 #include <stdint.h>
 
 int write_png(char *filename, uint32_t rows, uint32_t cols, float *buffer)
 {
-
+	return 0;
 }
 
 int main()
