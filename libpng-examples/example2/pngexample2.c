7152b5d551f5bf53ea9f53bd5e75d347fa6c6fcf
Gupta, Arjan <arjan.gupta@alibaba-inc.com>
2017-10-08 23:20:24 -0500

    Updates to example 2. Updates to gitignore

---
+++ libpng-examples/example2/pngexample2.c
@@ -0,0 +1,39 @@
+#include "../libpng-1.6.32/png.h"
+#include <stdint.h>
+#include <stdio.h>
+
+enum return_code
+{
+
+}
+
+int write_png(uint32_t rows, uint32_t cols, float *buffer)
+{
+	//declarations
+	FILE *fp = NULL;
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
+	png_bytep row = NULL;
+
+	//Open a file to be written to
+	fp = fopen("output.png", "wb");
+	if (fp == NULL)
+	{
+		return FILE_OPEN_FAILED;
+	}
+
+	return 100; //still implementing
+}
+
+int main()
+{
+	int rc = -1000;
+	float *buffer = NULL;
+	if( (rc =  write_png(1, 1, buffer)) == 0 )
+	{
+		printf("Example succeeded!\n");
+	} else {
+		printf("Example failed. rc = %d\n", rc);
+	}
+	return 0;
+}
