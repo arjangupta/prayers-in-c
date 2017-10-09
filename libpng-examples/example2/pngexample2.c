1fc77222f6a82072be9c8de4a30e004f8ffff99f
Gupta, Arjan <arjan.gupta@alibaba-inc.com>
2017-10-09 16:17:38 -0500

    Need a dylib for osx, add more functionality to example2

--- libpng-examples/example2/pngexample2.c
+++ libpng-examples/example2/pngexample2.c
@@ -1,14 +1,13 @@
-//#include "../libpng-1.6.32/png.h"
-
-#include <png.h>
-
+#include "../libpng-1.6.32/png.h"
 #include <stdint.h>
 #include <stdio.h>
 
 enum return_code
 {
-	FILE_OPEN_FAILED = -100
-
+	FILE_OPEN_FAILED = -100,
+	WRITE_STRUCT_ALLOC_FAILED, //-99
+	INFO_STRUCT_ALLOC_FAILED, //-98
+	PNG_CREATION_ERROR //-97
 };
 
 int write_png(uint32_t rows, uint32_t cols, float *buffer)
@@ -26,8 +25,30 @@ int write_png(uint32_t rows, uint32_t cols, float *buffer)
 		return FILE_OPEN_FAILED;
 	}
 
-	//Initialize the png structure
+	//Initialize the write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL)
+	{
+		return WRITE_STRUCT_ALLOC_FAILED;
+	}
 
+	//Initialize the info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if(info_ptr == NULL)
+	{
+		return INFO_STRUCT_ALLOC_FAILED;
+	}
+
+	//Set up exception handling
+	if (setjmp(png_jmpbuf(png_ptr)))
+	{
+		return PNG_CREATION_ERROR;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	//Write PNG header
+	
 
 	return 100; //still implementing
 }
