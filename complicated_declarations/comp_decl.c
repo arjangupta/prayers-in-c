643e9a00afd7c01f2a71152c3225a22b6f518ce0
arjangupta <arjangupta95@gmail.com>
2018-01-08 03:05:37 -0600

    Added another complicated decl

--- complicated_declarations/comp_decl.c
+++ complicated_declarations/comp_decl.c
@@ -9,5 +9,15 @@ int main()
    *	   typedefs for better readability.
    */ 
   void (* ( *f [5] ) (void) ) (void);
+  
+  /* TODO: Verify.
+   * My attempt at deciphering this: 
+   * This a function pointer called signal that takes in the following arguments:
+   * 1) an int
+   * 2) a function pointer that takes in an int and returns a double
+   * Additionally, signal will return a function pointer that takes in an int and a char as args, and will return a float. 
+   * Is that correct?
+   */
+  float ( * (*signal) ( int, double (*) (int) ) ) (int, char);
   return 0;
 }
