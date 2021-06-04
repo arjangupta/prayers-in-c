4340c8f22c11f77614cb9b2fb4fc3e5c4bb6c419
Arjan Gupta <agupta@intellifarms.com>
2021-06-04 13:22:42 -0500

    Optimize the standard bubble sort

--- bubble_sort_variation.c
+++ bubble_sort_variation.c
@@ -32,7 +32,7 @@ void bubble_sort(int* array, int length)
     int i, j, temp;
     for (i = 0; i < length; i++)
     {
-        for (j = 0; j < length - 1; j++)
+        for (j = 0; j < (length - (i + 1)); j++)
         {
             if (array[j] > array[j+1])
             {
